77432b98cb48492db206c0b05c60a9daf809ef66
happy <daisy831015@gmail.com>
2026-03-18 10:52:57 +0800

    新增首頁輪播功能，更新字型設定，調整樣式變數，並新增相關樣式檔案

--- src/layout/FrontLayout.jsx
+++ src/layout/FrontLayout.jsx
@@ -14,13 +14,14 @@ const FrontLayout = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container-lg">
           <Link className="navbar-brand" to="/">
-          <img src={`${import.meta.env.BASE_URL}logo-aloha.svg`} alt="index" />
+          <img src={`${import.meta.env.BASE_URL}logo-aloha.svg`} alt="logo-aloha" />
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
+              {/* 語言 */}
               <li className="nav-item dropdown">
                 <a 
                   className="nav-link dropdown-toggle" 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,60 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, A11y, Autoplay } from 'swiper/modules';
+
+// 引入樣式
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const Home = () => {
   return (
     <>
-    <p>首頁</p>
+    {/* 首頁主圖輪播 */}
+    <div className="positive-relative">
+      <Swiper
+        modules={[Navigation, A11y, Autoplay]}
+        spaceBetween={0} // 如果要做全螢幕輪播，通常設為 0
+        slidesPerView={1}
+        navigation={{
+          nextEl: '.swiper-button-next-aloha',
+          prevEl: '.swiper-button-prev-aloha',
+        }}
+        direction="horizontal"
+        loop={true}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        style={{height: '320px'}}
+      >
+        <SwiperSlide>
+          <img 
+          src={`${import.meta.env.BASE_URL}index-banner.jpg`}
+          alt="index-banner" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img 
+          src={`${import.meta.env.BASE_URL}index-banner.jpg`}
+          alt="index-banner" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img 
+          src={`${import.meta.env.BASE_URL}index-banner.jpg`}
+          alt="index-banner" />
+        </SwiperSlide>
+        {/* 遮罩層 */}
+        <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50 z-1"></div>
+        {/* 文字 */}
+        <div className="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center z-2">
+          <h1 className="display-5 fw-normal text-white">Discover your ideal hotel</h1>
+        </div>
+      </Swiper>
+
+      <div className="swiper-button-prev-aloha">
+        <span className="material-symbols-outlined display-5">chevron_left</span>
+      </div>
+      <div className="swiper-button-next-aloha">
+        <span className="material-symbols-outlined display-5">chevron_right</span>
+      </div>
+    </div>
     </>
   )
 }
